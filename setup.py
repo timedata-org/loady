e278b3e487502160e8fc7f735c696dde91a1e9fa
Tom Ritchford <tom@swirly.com>
2017-06-25 20:49:57 +0200

    Extract out VERSION logic

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
-import sys
-
-VERSION = 1.1
+import os, sys
 
 
 # From here: http://pytest.org/2.2.4/goodpractises.html
@@ -22,6 +20,21 @@ class RunTests(TestCommand):
             raise SystemExit(errno)
 
 
+NAME = 'gitty'
+OWNER = 'timedata-org'
+FILENAME = os.path.join(os.path.dirname(__file__), NAME, 'VERSION')
+VERSION = open(FILENAME).read().strip()
+
+URL = 'http://github.com/{OWNER}/{NAME}'.format(**locals())
+DOWNLOAD_URL = '{URL}/archive/{VERSION}.tar.gz'.format(**locals())
+
+with open('requirements.txt') as f:
+    INSTALL_REQUIRES = f.read().splitlines()
+
+with open('test_requirements.txt') as f:
+    TESTS_REQUIRE = f.read().splitlines()
+
+
 setup(
     name='Gitty',
     version=VERSION,
@@ -29,19 +42,20 @@ setup(
                  'dynamically from git'),
     author='Tom Ritchford',
     author_email='tom@swirly.com',
-    url='http://github.com/timedata-org/gitty/',
-    download_url='http://github.com/timedata-org/gitty/archive/1.1.tar.gz',
+    url=URL,
+    download_url=DOWNLOAD_URL,
     license='MIT',
     packages=find_packages(exclude=['test']),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
-    tests_require=['pytest'],
+    tests_require=TESTS_REQUIRE,
     cmdclass={'test': RunTests},
     keywords=['git', 'import'],
     include_package_data=True,
-    install_requires=['GitPython', 'requests'],
+    install_requires=INSTALL_REQUIRES,
 )
